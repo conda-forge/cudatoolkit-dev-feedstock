6ad982de009064f156ccf46b6910c56d402ac074
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-11-10 17:47:55 -0700

    increase build number

--- recipe/cudatoolkit-dev-post-install.py
+++ recipe/cudatoolkit-dev-post-install.py
@@ -290,8 +290,6 @@ def getplatform():
     plt = sys.platform
     if plt.startswith("linux"):
         return "linux"
-    elif plt.startswith("win"):
-        return "windows"
     elif plt.startswith("darwin"):
         return "osx"
     else:
@@ -301,7 +299,7 @@ def getplatform():
 def set_config():
     """Set necessary configurations"""
 
-    cudatoolkit = {"linux": {}, "windows": {}, "osx": {}}
+    cudatoolkit = {"linux": {}, "osx": {}}
     prefix = Path(os.environ["PREFIX"])
     extra_args = dict()
     with open(prefix / "bin" / "cudatoolkit-dev-extra-args.json", "r") as f:
@@ -326,10 +324,6 @@ def set_config():
         "blob": f'cuda_{cudatoolkit["version"]}.{cudatoolkit["version_build"]}_{cudatoolkit["driver_version"]}_linux'
     }
 
-    cudatoolkit["windows"] = {
-        "blob": f'cuda_{cudatoolkit["version"]}.{cudatoolkit["version_build"]}_windows'
-    }
-
     cudatoolkit["osx"] = {
         "blob": f'cuda_{cudatoolkit["version"]}.{cudatoolkit["version_build"]}_mac'
     }
@@ -337,7 +331,7 @@ def set_config():
     return cudatoolkit
 
 
-dispatcher = {"linux": LinuxExtractor, "windows": WindowsExtractor, "osx": OsxExtractor}
+dispatcher = {"linux": LinuxExtractor, "osx": OsxExtractor}
 
 
 def _main():
